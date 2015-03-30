352fbe44407f7b3868d3bfdea15814719c087399
chomscheid <conorhomscheid@conorhoeidsmbp2.dhcp.wustl.edu>
2015-03-30 18:51:00 -0500

    changes

---
+++ bin/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// check if fields passed are empty
+if(empty($_POST['name'])  		||
+   empty($_POST['phone']) 		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'chomscheid@gmail.com';
+$email_subject = "Conor's Portfolio Contact Form: New Message"; // 
+$email_body = "You have received a new message from your Portfolio's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@homscheid.com\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
